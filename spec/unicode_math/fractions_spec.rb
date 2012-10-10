cbc075eca5bc6057f73acad0b1549011832efca8
Steve Richert <steve.richert@gmail.com>
2012-10-10 15:49:19 -0400

    Ensure exact equality for fractions where possible

--- spec/unicode_math/fractions_spec.rb
+++ spec/unicode_math/fractions_spec.rb
@@ -3,23 +3,23 @@
 require 'spec_helper'
 
 describe UnicodeMath::Fractions do
+  it 'defines ¼ to equal 0.25' do
+    expect(¼).to eq(0.25)
+  end
+
+  it 'defines ⅓ to equal one third' do
+    expect(⅓).to be_within(0.00001).of(0.33333)
+  end
+
   it 'defines ½ to equal 0.5' do
     expect(½).to eq(0.5)
   end
 
-  it "defines ⅓ to equal one third" do
-    expect(⅓).to be_within(0.00001).of(0.33333)
-  end
-
-  it "defines ⅔ to equal two thirds" do
+  it 'defines ⅔ to equal two thirds' do
     expect(⅔).to be_within(0.00001).of(0.66666)
   end
 
-  it "defines ¼ to equal 0.25" do
-    expect(¼).to be_within(0.00001).of(0.25)
-  end
-
-  it "defines ¾ to equal 0.75" do
-    expect(¾).to be_within(0.00001).of(0.75)
+  it 'defines ¾ to equal 0.75' do
+    expect(¾).to eq(0.75)
   end
 end
