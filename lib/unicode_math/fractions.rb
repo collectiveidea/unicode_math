f984af29867862f4275da128aaa7ad1c27c31bb9
Daniel Morrison <daniel@collectiveidea.com>
2012-10-10 15:46:27 -0400

    More fractions!

--- lib/unicode_math/fractions.rb
+++ lib/unicode_math/fractions.rb
@@ -6,6 +6,22 @@ module UnicodeMath
         define_method "½" do
           0.5
         end
+
+        define_method "⅓" do
+          1.0 / 3.0
+        end
+
+        define_method "⅔" do
+          2.0 / 3.0
+        end
+
+        define_method "¼" do
+          0.25
+        end
+
+        define_method "¾" do
+          0.75
+        end
       end
     end
   end
--- spec/unicode_math/fractions_spec.rb
+++ spec/unicode_math/fractions_spec.rb
@@ -5,4 +5,20 @@ describe UnicodeMath::Fractions do
   it "defines ½ to equal 0.5" do
     expect(½).to be_within(0.00001).of(0.5)
   end
+
+  it "defines ⅓ to equal one third" do
+    expect(⅓).to be_within(0.00001).of(0.33333)
+  end
+
+  it "defines ⅔ to equal two thirds" do
+    expect(⅔).to be_within(0.00001).of(0.66666)
+  end
+
+  it "defines ¼ to equal 0.25" do
+    expect(¼).to be_within(0.00001).of(0.25)
+  end
+
+  it "defines ¾ to equal 0.75" do
+    expect(¾).to be_within(0.00001).of(0.75)
+  end
 end
