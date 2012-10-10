4f1a6457ff89924518e0f5fd5c8cd343c5c3d3b4
Steve Richert <steve.richert@gmail.com>
2012-10-10 15:51:27 -0400

    Whitespace and single quotes

--- lib/unicode_math/fractions.rb
+++ lib/unicode_math/fractions.rb
@@ -1,29 +1,31 @@
 # encoding: utf-8
+
 module UnicodeMath
   module Fractions
     def self.included(base)
       base.class_eval do
-        define_method "½" do
+        define_method '½' do
           0.5
         end
 
-        define_method "⅓" do
-          1.0 / 3.0
+        define_method '⅓' do
+          1.0 / 3
         end
 
-        define_method "⅔" do
-          2.0 / 3.0
+        define_method '⅔' do
+          2.0 / 3
         end
 
-        define_method "¼" do
+        define_method '¼' do
           0.25
         end
 
-        define_method "¾" do
+        define_method '¾' do
           0.75
         end
       end
     end
   end
 end
-Kernel.send :include, UnicodeMath::Fractions
+
+Kernel.send(:include, UnicodeMath::Fractions)
