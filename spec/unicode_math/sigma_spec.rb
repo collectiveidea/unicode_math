1c47b7257c3c429f6e632b4e8287b2fdf386eb8c
Ken Nishimura <ken97531@gmail.com>
2012-10-13 07:20:13 +0900

    Clarify some sigma specs

--- spec/unicode_math/sigma_spec.rb
+++ spec/unicode_math/sigma_spec.rb
@@ -4,8 +4,8 @@ require 'spec_helper'
 
 describe UnicodeMath::Sigma do
   it 'defines Σ to sum up elements of array' do
-    result = Σ [1,2,3]
-    expect(result).to eq(6)
+    result = Σ [1, 2, 3, 4]
+    expect(result).to eq(10)
   end
 
   it 'defines Σ to sum up elements of range' do
@@ -14,8 +14,8 @@ describe UnicodeMath::Sigma do
   end
 
   it 'defines Π to multiply elements of array' do
-    result = Π [1,2,3]
-    expect(result).to eq(6)
+    result = Π [1, 2, 3, 4]
+    expect(result).to eq(24)
   end
 
   it 'defines Π to multiply elements of range' do
