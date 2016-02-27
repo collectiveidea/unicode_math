30e82ac8ba7b19605e553a16b8e25a94c1b31767
Aditya Chowdhry <adityachowdhry09@gmail.com>
2016-02-27 23:19:06 +0530

    Added ⊂ , ⊃ ,∈ ,∉ , ∅

--- lib/unicode_math/set.rb
+++ lib/unicode_math/set.rb
@@ -11,9 +11,32 @@ module UnicodeMath
         define_method('∪') do |set|
           self | set
         end
+
+        define_method('⊂') do |set|
+          ((set.∩ self) == self)  
+        end
+
+        define_method('⊃') do |set|
+          ((self.∩ set) == set)  
+        end
+
+        define_method('∈') do |set|
+          (self.∩ set) == self
+        end
+
+        define_method('∉') do |set|
+          !((self.∩ set) == self)
+        end
+
+        define_method('∅') do
+          self.empty?
+        end
+
       end
     end
   end
 end
 
 Array.send(:include, UnicodeMath::Set)
+#Boolean.send(:include, UnicodeMath::Set)
+       
--- spec/unicode_math/set_spec.rb
+++ spec/unicode_math/set_spec.rb
@@ -6,6 +6,7 @@ describe UnicodeMath::Set do
   before do
     @primes   = [2, 3, 5, 7]
     @odd_nums = [3, 5, 7, 9]
+    @subset   = [3,5]
   end
 
   it 'defines the intersection operator: ∩' do
@@ -17,4 +18,58 @@ describe UnicodeMath::Set do
     union = @primes.∪ @odd_nums
     expect(union).to eq([2, 3, 5, 7, 9])
   end
+
+  it 'defines subset A ⊂ B : true' do 
+    subset = @subset.⊂ @primes
+    expect(subset).to eq(true)
+  end
+
+  it 'defines subset A ⊂ B : false' do 
+    subset = @primes.⊂ @subset
+    expect(subset).to eq(false)
+  end
+
+  it 'defines subset A ⊃ B : false' do 
+    subset = @subset.⊃ @primes
+    expect(subset).to eq(false)
+  end
+
+  it 'defines superset A ⊃ B : true' do 
+    subset = @primes.⊃ @subset
+    expect(subset).to eq(true)
+  end
+
+  it 'defines A belongs to B, A ∈ B : false' do 
+    belongs_to = @primes.∈ @subset
+    expect(belongs_to).to eq(false)
+  end
+
+  it 'defines A belongs to B, A ∈ B : true' do 
+    belongs_to = @subset.∈ @primes
+    expect(belongs_to).to eq(true)
+  end
+
+  it 'defines A does not belong to B, A ∉ B : false' do 
+    not_belongs_to = @subset.∉ @primes
+    #Does @subset does not belong to @primes ?
+    #True if it doesn't belong 
+    expect(not_belongs_to).to eq(false)
+  end
+
+  it 'defines A does not belong to B, A ∉ B : true' do 
+    not_belongs_to = @primes.∉ @subset
+    expect(not_belongs_to).to eq(true)
+  end
+
+  it 'defines A is empty' do 
+    empty = @primes.∅
+    expect(empty).to eq(false)
+  end
+
+  it 'defines A is empty' do 
+    empty = [].∅
+    expect(empty).to eq(true)
+  end
+
 end
+
