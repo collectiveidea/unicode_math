c803d66e3ff849416d4655a62a50a1170e4e9aad
Aditya Chowdhry <adityachowdhry09@gmail.com>
2016-02-27 23:34:11 +0530

    Clearer specs

--- spec/unicode_math/set_spec.rb
+++ spec/unicode_math/set_spec.rb
@@ -6,6 +6,7 @@ describe UnicodeMath::Set do
   before do
     @primes   = [2, 3, 5, 7]
     @odd_nums = [3, 5, 7, 9]
+    @superset = [1,2,3,4,5]
     @subset   = [3,5]
   end
 
@@ -20,44 +21,44 @@ describe UnicodeMath::Set do
   end
 
   it 'defines subset A ⊂ B : true' do 
-    subset = @subset.⊂ @primes
+    subset = @subset.⊂ @superset
     expect(subset).to eq(true)
   end
 
   it 'defines subset A ⊂ B : false' do 
-    subset = @primes.⊂ @subset
+    subset = @superset.⊂ @subset
     expect(subset).to eq(false)
   end
 
-  it 'defines subset A ⊃ B : false' do 
-    subset = @subset.⊃ @primes
+  it 'defines superset A ⊃ B : false' do 
+    subset = @subset.⊃ @superset
     expect(subset).to eq(false)
   end
 
   it 'defines superset A ⊃ B : true' do 
-    subset = @primes.⊃ @subset
+    subset = @superset.⊃ @subset
     expect(subset).to eq(true)
   end
 
   it 'defines A belongs to B, A ∈ B : false' do 
-    belongs_to = @primes.∈ @subset
+    belongs_to = @superset.∈ @subset
     expect(belongs_to).to eq(false)
   end
 
   it 'defines A belongs to B, A ∈ B : true' do 
-    belongs_to = @subset.∈ @primes
+    belongs_to = @subset.∈ @superset
     expect(belongs_to).to eq(true)
   end
 
   it 'defines A does not belong to B, A ∉ B : false' do 
-    not_belongs_to = @subset.∉ @primes
+    not_belongs_to = @subset.∉ @superset
     #Does @subset does not belong to @primes ?
     #True if it doesn't belong 
     expect(not_belongs_to).to eq(false)
   end
 
   it 'defines A does not belong to B, A ∉ B : true' do 
-    not_belongs_to = @primes.∉ @subset
+    not_belongs_to = @superset.∉ @subset
     expect(not_belongs_to).to eq(true)
   end
 
